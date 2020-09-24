cbb7fee93ece0952440b8929db3ea47b7f117697
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-24 15:14:28 +0800

    [mengqiu]  add feedback

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -6,6 +6,7 @@ jest.mock("axios");
 describe("users", () => {
   test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    // TODO feedback: 变量命名应是驼峰，mockdata -> mockData
     const mockdata = { name: "Jim", age: 18 };
     axios.get.mockResolvedValue({ data: mockdata });
     await expect(getUsers()).resolves.toEqual(mockdata);
